ea486994f0f4beafab92cc4c7a4fbb92f29df73b
Vune <Vune@gmail.com>
2022-09-15 01:54:51 +0700

    Setting Css default. Phân bố layout và tạo logic render. Chia nhỏ thânhf phần các layout, component,... Định tuyến router này kia

---
+++ src/routes/index.js
@@ -0,0 +1,19 @@
+import HomePage from '../pages/Home';
+import ExplorePage from '../pages/Explore';
+import FriendsPage from '../pages/Friends';
+import Profile from '../pages/Profile';
+import Upload from '../pages/Upload';
+
+//Layout
+import { HeaderOnly } from '../components/Layout';
+
+const publicRoutes = [
+    { path: '/', component: HomePage },
+    { path: '/explore', component: ExplorePage },
+    { path: '/friends', component: FriendsPage },
+    { path: '/profile', component: Profile },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+];
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
